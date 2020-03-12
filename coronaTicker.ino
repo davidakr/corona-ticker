993ffd41210c83fa3487c3f5e86bade18fdb9577
davidakr <30952483+davidakr@users.noreply.github.com>
2020-03-12 22:08:46 +0100

    Update coronaTicker.ino

--- coronaTicker.ino
+++ coronaTicker.ino
@@ -15,11 +15,6 @@ const char* host = "worldometers.info";
 void setup()
 {
   Serial.begin(115200);
-  // Serial.setDebugOutput(true);
-
-  Serial.println();
-  Serial.println();
-  Serial.println();
 
   for (uint8_t t = 4; t > 0; t--) {
     Serial.printf("[SETUP] WAIT %d...\n", t);
